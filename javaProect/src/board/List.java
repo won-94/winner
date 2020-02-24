4e860c6baef16cdaca2964d17eaaced1ec90a7d6
SIST165 <SIST165@DESKTOP-AC08R5J>
2020-02-24 10:35:51 +0900

    board 새로운 branch - List, Write 구현 후 커밋

---
+++ javaProect/src/board/List.java
@@ -0,0 +1,8 @@
+package board;
+
+public class List {
+
+	public static void main(String[] args) {
+		System.out.println("게시판 목록 페이지 구현 중...");
+	}
+}
---
+++ javaProect/src/board/Write.java
@@ -0,0 +1,8 @@
+package board;
+
+public class Write {
+
+	public static void main(String[] args) {
+		System.out.println("게시판 쓰기 페이지 구현 중...");
+	}
+}
