5146902ba73f97d0d843a67a9002e7bfc1a17345
SIST165 <SIST165@DESKTOP-AC08R5J>
2020-02-24 10:38:49 +0900

    경원 - 게시판 수정, 삭제 완료

---
+++ javaProect/src/board/Delete.java
@@ -0,0 +1,8 @@
+package board;
+
+public class Delete {
+
+	public static void main(String[] args) {
+		System.out.println("게시판 삭제 페이지 구현 중...");
+	}
+}
---
+++ javaProect/src/board/Edit.java
@@ -0,0 +1,8 @@
+package board;
+
+public class Edit {
+
+	public static void main(String[] args) {
+		System.out.println("게시판 수정 페이지 구현 중...");
+	}
+}
